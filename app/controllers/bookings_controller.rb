11fb6a9ba436a46423ae478cfb6c35328c813cbc
Mateo Sheshi <sheshi.mateo01@gmail.com>
2022-09-16 16:06:40 +0200

    chore: added passenger information fill

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,24 @@
 class BookingsController < ApplicationController
+  include BookingsHelper
+
   def new
     @booking = Booking.new
     @selected_flight = Flight.find(params[:flight])
-    @passenger_count = params[:passenger_count]
+    @passenger_count = params[:passenger_count].to_i
+    @passenger_count.times { @booking.passengers.build }
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_123320) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_124527) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "city"
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_16_123320) do
     t.index ["departure_id"], name: "index_flights_on_departure_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
   add_foreign_key "flights", "airports", column: "arrival_id"
   add_foreign_key "flights", "airports", column: "departure_id"
+  add_foreign_key "passengers", "bookings"
 end
